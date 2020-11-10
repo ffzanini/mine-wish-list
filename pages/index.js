02081ba7193cdc641cf87fb52b24362d459ba20e
Felipe Frantz Zanini <devffzanini@gmail.com>
2020-11-10 17:37:56 -0300

    Página inicial

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+
+function Home() {
+  return (
+    <main>
+    <div>Lista de desejos para a vida</div>
+    <ul>
+      <li>Viajar mais e conhecer paises como: Holanda, Canadá, Nova Zelândia, Itália, França, Irlanda, Alemanha e Noruega;</li>
+      <li>Escrever uma história e transforma-lá em um jogo;</li>
+      <li>Criar meu canal no youtube para programação e desenhos em pixelart;</li>
+      <li>Começar a fazer streams sobre tecnologia, desenhos e jogando com amigos.</li>
+    </ul>
+    </main>
+  );
+}
+
+export default Home;
