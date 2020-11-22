883d56ea4a35363e7a6a4567b97faa89e3b4d177
Felipe Frantz Zanini <devffzanini@gmail.com>
2020-11-22 17:31:39 -0300

    Lista atualizada

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,19 @@ function Home() {
     <main>
     <div>Lista de desejos para a vida</div>
     <ul>
-      <li>Viajar mais e conhecer paises como: Holanda, Canadá, Nova Zelândia, Itália, França, Irlanda, Alemanha e Noruega;</li>
-      <li>Escrever uma história e transforma-lá em um jogo;</li>
-      <li>Criar meu canal no youtube para programação e desenhos em pixelart;</li>
-      <li>Começar a fazer streams sobre tecnologia, desenhos e jogando com amigos.</li>
+      <li>Viajar sozinho e conhecer paises como: Holanda, Canadá, Nova Zelândia, Marrocos, Itália, França, Irlanda, Alemanha, Noruega, China e Japão</li>
+      <li>Saltar de paraquedas</li>
+      <li>Fazer bungee jump</li>
+      <li>Andar de asa delta</li>
+      <li>Esquiar</li>
+      <li>Praticar arco e flecha</li>
+      <li>Escrever uma história e transforma-lá em um jogo</li>
+      <li>Criar meu canal no youtube para programação, jogos e desenhos em pixelart</li>
+      <li>Começar a fazer streams sobre tecnologia, desenhos e jogando com amigos</li>
+      <li>Me mudar para uma cidade no litoral</li>
+      <li>Participar de mais projetos OpenSource para ajudar a comunidade de desenvolvimento</li>
+      <li>Faixa preta de Jiu Jitsu</li>
+      <li>Pegar pódio em um campeonato Sul Americano</li>
     </ul>
     </main>
   );
